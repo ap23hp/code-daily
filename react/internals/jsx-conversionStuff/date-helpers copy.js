efc683cd308a22d79a5102af41bc258d989d9115
Apra <aprakhanna5@gmail.com>
2026-01-10 20:34:37 +0530

    html to jsx conversion code

---
+++ react/internals/jsx-conversionStuff/date-helpers copy.js
@@ -0,0 +1,17 @@
+// import format from 'date-fns/format';
+
+/*
+  This convenient helper function takes an
+  ISO date string, and formats it like:
+  
+  March 14th, 2024 at 11:09 AM
+*/
+// export function formatDate(date) {
+//   return format(
+//     new Date(date),
+//     "MMMM do 'at' hh:mm b"
+//   );
+// }
+
+// console.log(formatDate('2026-01-10T11:09:00'));
+//If you write import something from 'package', that package must exist in node_modules
