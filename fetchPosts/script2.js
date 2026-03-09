be266e34d9ae87d4c710b49d3c4600dab8b81dba
ap23hp <aprakhanna5@gmail.com>
2026-03-09 09:36:33 +0530

    Added fetch posts and load more button functionality

--- fetchPosts/script2.js
+++ fetchPosts/script2.js
@@ -1,5 +1,9 @@
+let allPosts = [];
+let visiblePosts = 10;
+
 const postContainer = document.querySelector('#posts');
 const statusContainer = document.querySelector('#status');
+const loadMorebtn = document.querySelector('#loadMore');
 
 const url = `https://jsonplaceholder.typicode.com/posts`;
 
@@ -8,32 +12,47 @@ statusContainer.textContent = "Loading posts...";
 async function fetchPosts(url) {
   const response = await fetch(url);
   const data = await response.json();
-  return data;
+  allPosts = data;
 }
 
-function renderPosts(data) {
-  data.slice(0, 10).forEach((post) => {
+function renderPosts() {
+  postContainer.innerHTML = ""; // clear previous posts
+
+  allPosts.slice(0, visiblePosts).forEach((post) => {
+    const postWrapper = document.createElement('div');
+
     const postTitle = document.createElement('h2');
     postTitle.textContent = post.title;
 
     const postBody = document.createElement('p');
     postBody.textContent = post.body;
 
-    postContainer.appendChild(postTitle);
-    postContainer.appendChild(postBody);
+    postWrapper.appendChild(postTitle);
+    postWrapper.appendChild(postBody);
+
+    postContainer.appendChild(postWrapper);
   });
 }
 
 async function main() {
   try {
-    const data = await fetchPosts(url);
+    await fetchPosts(url);
     statusContainer.textContent = "";
-    renderPosts(data);
+    renderPosts();
   } catch (error) {
     statusContainer.textContent = "Failed to load posts";
   }
 }
 
 main();
+
+loadMorebtn.addEventListener("click", function () {
+  visiblePosts += 10;
+  renderPosts();
+
+  if (visiblePosts >= allPosts.length) {
+    loadMorebtn.style.display = "none";
+  }
+});
 
 //fetchPosts() returns a Promise that resolves to the posts data, and we use await when calling it so we get the resolved data and can pass it to renderPosts().
