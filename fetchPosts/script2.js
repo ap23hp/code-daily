ec22d628adb7f0333b2648d7a1f3aefa727c1135
ap23hp <aprakhanna5@gmail.com>
2026-03-09 09:43:43 +0530

    notes related to fetch

--- fetchPosts/script2.js
+++ fetchPosts/script2.js
@@ -56,3 +56,17 @@ loadMorebtn.addEventListener("click", function () {
 });
 
 //fetchPosts() returns a Promise that resolves to the posts data, and we use await when calling it so we get the resolved data and can pass it to renderPosts().
+//When we use fetch(): fetch(url) sends a request to the server.
+//It returns a Promise because the browser needs time to contact the server.
+//So:fetch(url) -> Promise -> resolves to → Response object
+//The Response object contains information about the request.
+//Example things inside it:status, headers, body,ok
+//But the actual data is still inside the body and 
+// not directly usable yet.To extract the data, we use:response.json()
+// This reads the response body and converts it into JavaScript data.
+// But this also takes time, so it returns another Promise.
+// Final result becomes:
+// Which is a JavaScript array of objects.
+// Why We Use await Twice
+//Because there are two asynchronous steps:
+//await fetch(url) await response.json()
