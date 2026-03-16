23e0c21b4d9a8760486ecc1d6249de54026816ba
ap23hp <aprakhanna5@gmail.com>
2026-03-16 08:16:28 +0530

    loading and error states

--- search-live/script.js
+++ search-live/script.js
@@ -1,6 +1,7 @@
 const listCont = document.querySelector("#post-list-container")
 const searchInput=document.querySelector("#sea")
 let allPosts=[]
+let timer;
 async function fetchPosts(){
 const response= await fetch("https://jsonplaceholder.typicode.com/posts")
 const data= await response.json()
@@ -10,12 +11,21 @@ return allPosts
 }
 
 searchInput.addEventListener("input",function(){
- let filteredposts= allPosts.filter((post)=>{
-return post.title.toLowerCase().includes(searchInput.value.toLowerCase())
-})
-renderList(filteredposts)
+      clearTimeout(timer)
+
+  timer = setTimeout(()=>{
+
+    const searchText = searchInput.value.toLowerCase()
+
+    const filteredPosts = allPosts.filter((post)=>{
+      return post.title.toLowerCase().includes(searchText)
     })
 
+    renderList(filteredPosts)
+
+  },500)
+
+})
 
 function renderList(posts){
     listCont.innerHTML = "";
@@ -28,3 +38,8 @@ function renderList(posts){
 }
 
 fetchPosts()
+//debounce, on every search filter is running, not good for performance so we cand elay search by using timers when user search
+
+// Search runs only after 500ms delay
+// 2️⃣ If user keeps typing → previous timer is cancelled
+// 3️⃣ Filtering happens inside the timer
