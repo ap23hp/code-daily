7f59662b4a7c1fa44e9c45bddc408cb904ba8578
ap23hp <aprakhanna5@gmail.com>
2026-03-19 13:01:50 +0530

    helper functiona and shortcodes

--- php/practice.php
+++ php/practice.php
@@ -16,9 +16,14 @@ function milani_register_cpt(){
         'label'=>'Cities',
         'rewrite'=>['slug'=>"", 'with_front'=>false],
     ]);
+       register_post_type("city_service", [
+        'public' =>true,
+        'label'=>'city_services',
+        'rewrite'=>['slug'=>'', 'with_front'=>false],
+    ]);
 }
 
-add_action('init','milani_register_cpt')
+add_action('init','milani_register_cpt');
 
 
 // foreach( $cities as $slug => $data ) {
@@ -33,11 +38,30 @@ function milani_get_current_city() {
     return $parts[0];
 }
 add_shortcode('milani_city',function(){
-$cities=milani_get_all_cities()
-$slug=milani_get_current_city()
+$cities=milani_get_all_cities();
+$slug=milani_get_current_city();
+return $milani_city=$cities[$slug]['name'];
+})
 
-   return $milani_city=$cities[$slug]['name']
+function milani_get_current_service(){
+    $path=trim($_SERVER['REQUEST_URI'],"/");
+    $parts=explode('/',$path);
+    return $parts[1];
+}
+function milani_get_all_services(){
+    return [
+      'electric-charger' => ['title' => 'EV Charger Installation'],
+    ];
+}
 
+add_shortcode('milani_phone',function(){
+    $cities=milani_get_all_cities();
+$slug=milani_get_current_city();
+return $milani_phone=$cities[$slug]['phone'];
+})
 
-
+add_shortcode('milani_service', function(){
+        $services=milani_get_all_services();
+$slug=milani_get_current_service();
+return $milani_service=$services[$slug]['title'];
 })
